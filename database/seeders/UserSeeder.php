02fc388323e1f0df03107bd87c3f7d75fa89ed6b
Alfredo Ortegón Sepúlveda <alorse@gmail.com>
2020-12-16 10:56:05 -0500

    Modify Users actions to get username field.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,6 +18,7 @@ class UserSeeder extends Seeder
         //Create and basic user
         User::create([
             'name' => 'Admin Exenta',
+            'username' => '@admin',
             'email' => 'admin@mail.com',
             'password' => '$2y$10$MgW98d2i9JZ/jEePgqFys.g5ZZozoKU.9yhW/t9DE8gF5a2lkM8Xm', // 12345678
         ]);
@@ -28,6 +29,7 @@ class UserSeeder extends Seeder
         for ($i = 1; $i <= $total_users; $i++) {
             User::create([
                 'name' => $faker->name,
+                'username' => '@' . $faker->userName,
                 'email' => $faker->unique()->safeEmail,
                 'password' => Hash::make('password'),
             ]);
