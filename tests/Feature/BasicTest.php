f887a0f981a1540279519cc3ac4b1982dd33ca78
Alfredo Ortegón Sepúlveda <alorse@gmail.com>
2020-12-16 15:24:58 -0500

    Register user test.

---
+++ tests/Feature/BasicTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class BasicTest extends TestCase
+{
+    public function testRegisterTest()
+    {
+        $faker = \Faker\Factory::create();
+        $name = $faker->name;
+        $username = $faker->username;
+        $mail = $faker->unique()->safeEmail;
+        $pass = Hash::make('password');
+        $user = [
+            'name' => $name,
+            'username' => $username,
+            'email' => $mail,
+            'password' => $pass
+        ];
+    
+        $response = $this->post('/register', $user);
+        // $this->assertAuthenticatedAs($user);
+
+    
+        $this->assertDatabaseHas('users', $user);
+    }
+
+}
